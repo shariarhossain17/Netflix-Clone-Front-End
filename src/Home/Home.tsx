d264b147024af3fec57aecea2089b26f85db3821
shariarhossain23e <shariarhossain23@gamil.comm>
2022-10-04 19:42:32 +0600

    navbar

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,6 +1,16 @@
+import TopBar from "../components/TopBar/TopBar";
 import "./Home.scss";
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <TopBar />
+      <img
+        width="100%"
+        src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+        alt=""
+      />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/TopBar/TopBar.tsx
+++ src/components/TopBar/TopBar.tsx
@@ -1,6 +1,39 @@
+import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
 import "./Topbar.scss";
 const TopBar = () => {
-  return <div></div>;
+  return (
+    <div className="topbar">
+      <div className="container">
+        <div className="left">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+            alt=""
+          />
+          <span>Homepage</span>
+          <span>Series</span>
+          <span>Movies</span>
+          <span>New and Popular</span>
+          <span>My List</span>
+        </div>
+        <div className="right">
+          <Search className="icon" />
+          <span>KID</span>
+          <Notifications />
+          <img
+            src="https://img.freepik.com/free-photo/portrait-successful-man-having-stubble-posing-with-broad-smile-keeping-arms-folded_171337-1267.jpg?w=2000"
+            alt=""
+          />
+          <div className="profile">
+            <ArrowDropDown className="icon" />
+            <div className="options">
+              <span>Settings</span>
+              <span>Logout</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TopBar;
