2eace1ca72c63de4d4ad93e8740a71efdfc329ac
shariarhossain23e <shariarhossain23@gamil.comm>
2022-10-04 18:56:46 +0600

    setup front end

---
+++ src/Home/Home.tsx
@@ -0,0 +1,6 @@
+import "./Home.scss";
+const Home = () => {
+  return <div className="home"></div>;
+};
+
+export default Home;
---
+++ src/components/TopBar/TopBar.tsx
@@ -0,0 +1,6 @@
+import "./Topbar.scss";
+const TopBar = () => {
+  return <div></div>;
+};
+
+export default TopBar;
