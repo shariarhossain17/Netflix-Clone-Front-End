e095f8a4280294a0f0cf2ab2e13cfdb863213005
shariarhossain23e <shariarhossain23@gamil.comm>
2022-10-05 01:23:24 +0600

    list slider added

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -2,15 +2,25 @@ import {
   ArrowBackIosOutlined,
   ArrowForwardIosOutlined,
 } from "@material-ui/icons";
+import { useRef } from "react";
 import ListItem from "../ListItem/ListItem";
 import "./List.scss";
 const List = () => {
+  let listRef = useRef<HTMLDivElement>(null);
+  const handleClick = (slider: string) => {
+    if (slider === "left" && listRef.current !== null) {
+      listRef.current.style.transform = `translateX(230px)`;
+    }
+  };
   return (
     <div className="list">
       <span className="listTitle">Continue to watch</span>
       <div className="wrapper">
-        <ArrowBackIosOutlined className="sliderArrow left" />
-        <div className="container">
+        <ArrowBackIosOutlined
+          onClick={() => handleClick("left")}
+          className="sliderArrow left"
+        />
+        <div className="container" ref={listRef}>
           <ListItem />
           <ListItem />
           <ListItem />
@@ -23,7 +33,10 @@ const List = () => {
           <ListItem />
           <ListItem />
         </div>
-        <ArrowForwardIosOutlined className="sliderArrow right" />
+        <ArrowForwardIosOutlined
+          onClick={() => handleClick("right")}
+          className="sliderArrow right"
+        />
       </div>
     </div>
   );
